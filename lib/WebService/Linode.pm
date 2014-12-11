c08562a43cc9cb2ff0e759a291e737ac6907572c
James Ottinger <jottinger@linode.com>
2014-12-11 10:51:47 -0500

    update WebService::Linode to version 0.25

--- lib/WebService/Linode.pm
+++ lib/WebService/Linode.pm
@@ -9,7 +9,7 @@ use Carp;
 use List::Util qw(first);
 use WebService::Linode::Base;
 
-our $VERSION = '0.20';
+our $VERSION = '0.25';
 our @ISA     = ("WebService::Linode::Base");
 our $AUTOLOAD;
 
@@ -28,6 +28,7 @@ my %validation = (
         distributions => [ [], [ 'distributionid' ] ],
         kernels => [ [], [ 'isxen', 'kernelid' ] ],
         linodeplans => [ [], [ 'planid' ] ],
+        nodebalancers => [ [], [] ],
         stackscripts => [ [], [qw( distributionid distributionvendor keywords )] ],
     },
     domain => {
@@ -41,6 +42,10 @@ my %validation = (
         delete => [ [ 'domainid', 'resourceid' ], [] ],
         list => [ [ 'domainid' ], [ 'resourceid' ] ],
         update => [ [ 'resourceid' ], [qw( domainid name port priority protocol target ttl_sec weight )] ],
+    },
+    image => {
+        delete => [ [ 'imageid' ], [] ],
+        list => [ [], [ 'imageid', 'pending' ] ],
     },
     linode => {
         boot => [ [ 'linodeid' ], [ 'configid' ] ],
@@ -56,17 +61,19 @@ my %validation = (
         webconsoletoken => [ [ 'linodeid' ], [] ],
     },
     linode_config => {
-        create => [ [qw( kernelid label linodeid )], [qw( comments devtmpfs_automount disklist helper_depmod helper_disableupdatedb helper_xen ramlimit rootdevicecustom rootdevicenum rootdevicero runlevel )] ],
+        create => [ [qw( kernelid label linodeid )], [qw( comments devtmpfs_automount disklist helper_depmod helper_disableupdatedb helper_network helper_xen ramlimit rootdevicecustom rootdevicenum rootdevicero runlevel )] ],
         delete => [ [ 'configid', 'linodeid' ], [] ],
         list => [ [ 'linodeid' ], [ 'configid' ] ],
-        update => [ [ 'configid' ], [qw( comments devtmpfs_automount disklist helper_depmod helper_disableupdatedb helper_xen kernelid label linodeid ramlimit rootdevicecustom rootdevicenum rootdevicero runlevel )] ],
+        update => [ [ 'configid' ], [qw( comments devtmpfs_automount disklist helper_depmod helper_disableupdatedb helper_network helper_xen kernelid label linodeid ramlimit rootdevicecustom rootdevicenum rootdevicero runlevel )] ],
     },
     linode_disk => {
-        create => [ [qw( label linodeid size type )], [] ],
+        create => [ [qw( label linodeid size type )], [qw( fromdistributionid isreadonly rootpass rootsshkey )] ],
         createfromdistribution => [ [qw( distributionid label linodeid rootpass size )], [ 'rootsshkey' ] ],
-        createfromstackscript => [ [qw( distributionid label linodeid rootpass size stackscriptid stackscriptudfresponses )], [] ],
+        createfromimage => [ [ 'imageid', 'linodeid' ], [qw( label rootpass rootsshkey size )] ],
+        createfromstackscript => [ [qw( distributionid label linodeid rootpass size stackscriptid stackscriptudfresponses )], [ 'rootsshkey' ] ],
         delete => [ [ 'diskid', 'linodeid' ], [] ],
         duplicate => [ [ 'diskid', 'linodeid' ], [] ],
+        imagize => [ [ 'diskid', 'linodeid' ], [ 'description', 'label' ] ],
         list => [ [ 'linodeid' ], [ 'diskid' ] ],
         resize => [ [qw( diskid linodeid size )], [] ],
         update => [ [ 'diskid' ], [qw( isreadonly label linodeid )] ],
@@ -75,13 +82,14 @@ my %validation = (
         addprivate => [ [ 'linodeid' ], [] ],
         addpublic => [ [ 'linodeid' ], [] ],
         list => [ [ 'linodeid' ], [ 'ipaddressid' ] ],
+        setrdns => [ [ 'hostname', 'ipaddressid' ], [] ],
         swap => [ [ 'ipaddressid' ], [ 'tolinodeid', 'withipaddressid' ] ],
     },
     linode_job => {
         list => [ [ 'linodeid' ], [ 'jobid', 'pendingonly' ] ],
     },
     nodebalancer => {
-        create => [ [ 'datacenterid', 'paymentterm' ], [ 'clientconnthrottle', 'label' ] ],
+        create => [ [ 'datacenterid' ], [qw( clientconnthrottle label paymentterm )] ],
         delete => [ [ 'nodebalancerid' ], [] ],
         list => [ [], [ 'nodebalancerid' ] ],
         update => [ [ 'nodebalancerid' ], [ 'clientconnthrottle', 'label' ] ],
@@ -115,8 +123,8 @@ my %validation = (
 sub AUTOLOAD {
     ( my $name = $AUTOLOAD ) =~ s/.+:://;
     return if $name eq 'DESTROY';
-    if ( $name =~ m/^(.*?)_([^_]+)$/ ) {
-        my ( $thing, $action ) = ( $1, $2 );
+    if ( $name =~ m/^(QUEUE_)?(.*?)_([^_]+)$/ ) {
+        my ( $queue, $thing, $action ) = ( $1, $2, $3 );
         if ( exists $validation{$thing} && exists $validation{$thing}{$action} )
         {   no strict 'refs';
             *{$AUTOLOAD} = sub {
@@ -137,6 +145,7 @@ sub AUTOLOAD {
                     }
                 }
                 ( my $apiAction = "${thing}_${action}" ) =~ s/_/./g;
+                return $self->queue_request( api_action => $apiAction, %args ) if $queue;
                 my $data = $self->do_request( api_action => $apiAction, %args );
                 return [ map { $self->_lc_keys($_) } @$data ]
                     if ref $data eq 'ARRAY';
@@ -152,6 +161,29 @@ sub AUTOLOAD {
         return;
     }
     croak "Undefined subroutine \&$AUTOLOAD called";
+}
+
+sub send_queued_requests {
+    my $self = shift;
+    my $items = shift;
+
+    if ( $self->list_queue == 0 ) {
+        $self->_error( -1, "No queued items to send" );
+        return;
+    }
+
+    my @responses;
+    for my $data ( $self->process_queue( $items ) ) {
+        if ( ref $data eq 'ARRAY' ) {
+            push @responses, [ map { $self->_lc_keys($_) } @$data ];
+        } elsif( ref $data eq 'HASH' ) {
+            push @responses, $self->_lc_keys($data);
+        } else {
+            push @responses, $data;
+        }
+    }
+
+    return @responses;
 }
 
 'mmm, cake';
@@ -177,19 +209,41 @@ same.  For additional information see L<http://www.linode.com/api/>
 For documentation of possible arguments to the constructor, see
 L<WebService::Linode::Base>.
 
+=head1 Batch requests
+
+Each of the Linode API methods below may optionally be prefixed with QUEUE_
+to add that request to a queue to be processed later in one or more batch
+requests which can be processed by calling send_queued_requests.
+For example:
+
+    my @linode_ids = () # Get your linode ids through normal methods
+    my @responses = map { $api->linode_ip_list( linodeid=>$_ ) } @linode_ids;
+
+Can be reduced to a single request:
+
+    my @linode_ids = () # Get your linode ids through normal methods
+    $api->QUEUE_linode_ip_list( linodeid=>$_ ) for @linode_ids;
+    my @responses = $api->send_queued_requests; # One api request
+
+See L<WebService::Linode::Base> for additional queue management methods.
+
+=head3 send_queued_requests
+
+Send queued batch requests, returns list of responses.
+
 =head1 Methods from the Linode API
 
-=head3 avail_stackscripts
+=head3 avail_datacenters
+
+=head3 avail_nodebalancers
+
+=head3 avail_distributions
 
 Optional Parameters:
 
 =over 4
 
 =item * distributionid
-
-=item * distributionvendor
-
-=item * keywords
 
 =back
 
@@ -215,15 +269,67 @@ Optional Parameters:
 
 =back
 
-=head3 avail_datacenters
-
-=head3 avail_distributions
+=head3 avail_stackscripts
 
 Optional Parameters:
 
 =over 4
 
 =item * distributionid
+
+=item * distributionvendor
+
+=item * keywords
+
+=back
+
+=head3 domain_list
+
+Optional Parameters:
+
+=over 4
+
+=item * domainid
+
+=back
+
+=head3 domain_update
+
+Required Parameters:
+
+=over 4
+
+=item * domainid
+
+=back
+
+Optional Parameters:
+
+=over 4
+
+=item * axfr_ips
+
+=item * description
+
+=item * domain
+
+=item * expire_sec
+
+=item * lpm_displaygroup
+
+=item * master_ips
+
+=item * refresh_sec
+
+=item * retry_sec
+
+=item * soa_email
+
+=item * status
+
+=item * ttl_sec
+
+=item * type
 
 =back
 
@@ -272,100 +378,6 @@ Required Parameters:
 =over 4
 
 =item * domainid
-
-=back
-
-=head3 domain_update
-
-Required Parameters:
-
-=over 4
-
-=item * domainid
-
-=back
-
-Optional Parameters:
-
-=over 4
-
-=item * axfr_ips
-
-=item * description
-
-=item * domain
-
-=item * expire_sec
-
-=item * lpm_displaygroup
-
-=item * master_ips
-
-=item * refresh_sec
-
-=item * retry_sec
-
-=item * soa_email
-
-=item * status
-
-=item * ttl_sec
-
-=item * type
-
-=back
-
-=head3 domain_list
-
-Optional Parameters:
-
-=over 4
-
-=item * domainid
-
-=back
-
-=head3 domain_resource_create
-
-Required Parameters:
-
-=over 4
-
-=item * domainid
-
-=item * type
-
-=back
-
-Optional Parameters:
-
-=over 4
-
-=item * name
-
-=item * port
-
-=item * priority
-
-=item * protocol
-
-=item * target
-
-=item * ttl_sec
-
-=item * weight
-
-=back
-
-=head3 domain_resource_delete
-
-Required Parameters:
-
-=over 4
-
-=item * domainid
-
-=item * resourceid
 
 =back
 
@@ -419,39 +431,65 @@ Optional Parameters:
 
 =back
 
-=head3 linode_mutate
+=head3 domain_resource_delete
+
+Required Parameters:
+
+=over 4
+
+=item * domainid
+
+=item * resourceid
+
+=back
+
+=head3 domain_resource_create
+
+Required Parameters:
+
+=over 4
+
+=item * domainid
+
+=item * type
+
+=back
+
+Optional Parameters:
+
+=over 4
+
+=item * name
+
+=item * port
+
+=item * priority
+
+=item * protocol
+
+=item * target
+
+=item * ttl_sec
+
+=item * weight
+
+=back
+
+=head3 linode_resize
 
 Required Parameters:
 
 =over 4
 
 =item * linodeid
-
-=back
-
-=head3 linode_create
-
-Required Parameters:
-
-=over 4
-
-=item * datacenterid
 
 =item * planid
 
 =back
 
+=head3 linode_list
+
 Optional Parameters:
-
-=over 4
-
-=item * paymentterm
-
-=back
-
-=head3 linode_reboot
-
-Required Parameters:
 
 =over 4
 
@@ -459,15 +497,7 @@ Required Parameters:
 
 =back
 
-Optional Parameters:
-
-=over 4
-
-=item * configid
-
-=back
-
-=head3 linode_webconsoletoken
+=head3 linode_mutate
 
 Required Parameters:
 
@@ -495,15 +525,23 @@ Optional Parameters:
 
 =back
 
-=head3 linode_resize
+=head3 linode_create
 
 Required Parameters:
 
 =over 4
 
-=item * linodeid
+=item * datacenterid
 
 =item * planid
+
+=back
+
+Optional Parameters:
+
+=over 4
+
+=item * paymentterm
 
 =back
 
@@ -526,34 +564,6 @@ Optional Parameters:
 =over 4
 
 =item * paymentterm
-
-=back
-
-=head3 linode_shutdown
-
-Required Parameters:
-
-=over 4
-
-=item * linodeid
-
-=back
-
-=head3 linode_delete
-
-Required Parameters:
-
-=over 4
-
-=item * linodeid
-
-=back
-
-Optional Parameters:
-
-=over 4
-
-=item * skipchecks
 
 =back
 
@@ -611,11 +621,69 @@ Optional Parameters:
 
 =back
 
-=head3 linode_list
+=head3 linode_webconsoletoken
+
+Required Parameters:
+
+=over 4
+
+=item * linodeid
+
+=back
+
+=head3 linode_reboot
+
+Required Parameters:
+
+=over 4
+
+=item * linodeid
+
+=back
 
 Optional Parameters:
 
 =over 4
+
+=item * configid
+
+=back
+
+=head3 linode_shutdown
+
+Required Parameters:
+
+=over 4
+
+=item * linodeid
+
+=back
+
+=head3 linode_delete
+
+Required Parameters:
+
+=over 4
+
+=item * linodeid
+
+=back
+
+Optional Parameters:
+
+=over 4
+
+=item * skipchecks
+
+=back
+
+=head3 linode_config_delete
+
+Required Parameters:
+
+=over 4
+
+=item * configid
 
 =item * linodeid
 
@@ -649,6 +717,8 @@ Optional Parameters:
 
 =item * helper_disableupdatedb
 
+=item * helper_network
+
 =item * helper_xen
 
 =item * ramlimit
@@ -660,18 +730,6 @@ Optional Parameters:
 =item * rootdevicero
 
 =item * runlevel
-
-=back
-
-=head3 linode_config_delete
-
-Required Parameters:
-
-=over 4
-
-=item * configid
-
-=item * linodeid
 
 =back
 
@@ -698,6 +756,8 @@ Optional Parameters:
 =item * helper_depmod
 
 =item * helper_disableupdatedb
+
+=item * helper_network
 
 =item * helper_xen
 
@@ -737,59 +797,15 @@ Optional Parameters:
 
 =back
 
-=head3 linode_disk_create
+=head3 linode_disk_createfromimage
 
 Required Parameters:
 
 =over 4
 
-=item * label
+=item * imageid
 
 =item * linodeid
-
-=item * size
-
-=item * type
-
-=back
-
-=head3 linode_disk_createfromstackscript
-
-Required Parameters:
-
-=over 4
-
-=item * distributionid
-
-=item * label
-
-=item * linodeid
-
-=item * rootpass
-
-=item * size
-
-=item * stackscriptid
-
-=item * stackscriptudfresponses
-
-=back
-
-=head3 linode_disk_createfromdistribution
-
-Required Parameters:
-
-=over 4
-
-=item * distributionid
-
-=item * label
-
-=item * linodeid
-
-=item * rootpass
-
-=item * size
 
 =back
 
@@ -797,37 +813,17 @@ Optional Parameters:
 
 =over 4
 
+=item * label
+
+=item * rootpass
+
 =item * rootsshkey
-
-=back
-
-=head3 linode_disk_resize
-
-Required Parameters:
-
-=over 4
-
-=item * diskid
-
-=item * linodeid
 
 =item * size
 
 =back
 
 =head3 linode_disk_duplicate
-
-Required Parameters:
-
-=over 4
-
-=item * diskid
-
-=item * linodeid
-
-=back
-
-=head3 linode_disk_delete
 
 Required Parameters:
 
@@ -861,6 +857,84 @@ Optional Parameters:
 
 =back
 
+=head3 linode_disk_createfromstackscript
+
+Required Parameters:
+
+=over 4
+
+=item * distributionid
+
+=item * label
+
+=item * linodeid
+
+=item * rootpass
+
+=item * size
+
+=item * stackscriptid
+
+=item * stackscriptudfresponses
+
+=back
+
+Optional Parameters:
+
+=over 4
+
+=item * rootsshkey
+
+=back
+
+=head3 linode_disk_imagize
+
+Required Parameters:
+
+=over 4
+
+=item * diskid
+
+=item * linodeid
+
+=back
+
+Optional Parameters:
+
+=over 4
+
+=item * description
+
+=item * label
+
+=back
+
+=head3 linode_disk_delete
+
+Required Parameters:
+
+=over 4
+
+=item * diskid
+
+=item * linodeid
+
+=back
+
+=head3 linode_disk_resize
+
+Required Parameters:
+
+=over 4
+
+=item * diskid
+
+=item * linodeid
+
+=item * size
+
+=back
+
 =head3 linode_disk_list
 
 Required Parameters:
@@ -876,6 +950,74 @@ Optional Parameters:
 =over 4
 
 =item * diskid
+
+=back
+
+=head3 linode_disk_createfromdistribution
+
+Required Parameters:
+
+=over 4
+
+=item * distributionid
+
+=item * label
+
+=item * linodeid
+
+=item * rootpass
+
+=item * size
+
+=back
+
+Optional Parameters:
+
+=over 4
+
+=item * rootsshkey
+
+=back
+
+=head3 linode_disk_create
+
+Required Parameters:
+
+=over 4
+
+=item * label
+
+=item * linodeid
+
+=item * size
+
+=item * type
+
+=back
+
+Optional Parameters:
+
+=over 4
+
+=item * fromdistributionid
+
+=item * isreadonly
+
+=item * rootpass
+
+=item * rootsshkey
+
+=back
+
+=head3 linode_ip_setrdns
+
+Required Parameters:
+
+=over 4
+
+=item * hostname
+
+=item * ipaddressid
 
 =back
 
@@ -909,16 +1051,6 @@ Required Parameters:
 
 =back
 
-=head3 linode_ip_addpublic
-
-Required Parameters:
-
-=over 4
-
-=item * linodeid
-
-=back
-
 =head3 linode_ip_list
 
 Required Parameters:
@@ -934,6 +1066,16 @@ Optional Parameters:
 =over 4
 
 =item * ipaddressid
+
+=back
+
+=head3 linode_ip_addpublic
+
+Required Parameters:
+
+=over 4
+
+=item * linodeid
 
 =back
 
@@ -954,6 +1096,66 @@ Optional Parameters:
 =item * jobid
 
 =item * pendingonly
+
+=back
+
+=head3 image_delete
+
+Required Parameters:
+
+=over 4
+
+=item * imageid
+
+=back
+
+=head3 image_list
+
+Optional Parameters:
+
+=over 4
+
+=item * imageid
+
+=item * pending
+
+=back
+
+=head3 stackscript_list
+
+Optional Parameters:
+
+=over 4
+
+=item * stackscriptid
+
+=back
+
+=head3 stackscript_update
+
+Required Parameters:
+
+=over 4
+
+=item * stackscriptid
+
+=back
+
+Optional Parameters:
+
+=over 4
+
+=item * description
+
+=item * distributionidlist
+
+=item * ispublic
+
+=item * label
+
+=item * rev_note
+
+=item * script
 
 =back
 
@@ -993,41 +1195,15 @@ Required Parameters:
 
 =back
 
-=head3 stackscript_update
+=head3 nodebalancer_config_delete
 
 Required Parameters:
 
 =over 4
 
-=item * stackscriptid
+=item * configid
 
-=back
-
-Optional Parameters:
-
-=over 4
-
-=item * description
-
-=item * distributionidlist
-
-=item * ispublic
-
-=item * label
-
-=item * rev_note
-
-=item * script
-
-=back
-
-=head3 stackscript_list
-
-Optional Parameters:
-
-=over 4
-
-=item * stackscriptid
+=item * nodebalancerid
 
 =back
 
@@ -1068,18 +1244,6 @@ Optional Parameters:
 =item * ssl_key
 
 =item * stickiness
-
-=back
-
-=head3 nodebalancer_config_delete
-
-Required Parameters:
-
-=over 4
-
-=item * configid
-
-=item * nodebalancerid
 
 =back
 
@@ -1175,6 +1339,24 @@ Required Parameters:
 
 =back
 
+=head3 nodebalancer_node_list
+
+Required Parameters:
+
+=over 4
+
+=item * configid
+
+=back
+
+Optional Parameters:
+
+=over 4
+
+=item * nodeid
+
+=back
+
 =head3 nodebalancer_node_update
 
 Required Parameters:
@@ -1196,24 +1378,6 @@ Optional Parameters:
 =item * mode
 
 =item * weight
-
-=back
-
-=head3 nodebalancer_node_list
-
-Required Parameters:
-
-=over 4
-
-=item * configid
-
-=back
-
-Optional Parameters:
-
-=over 4
-
-=item * nodeid
 
 =back
 
@@ -1245,15 +1409,16 @@ Optional Parameters:
 
 =over
 
-=item * Michael Greb, C<< <mgreb@linode.com> >>
+=item * Michael Greb, C<< <michael@thegrebs.com> >>
 
-=item * Stan "The Man" Schwertly C<< <stan@linode.com> >>
+=item * Stan "The Man" Schwertly C<< <stan@schwertly.com> >>
 
 =back
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2008-2009 Linode, LLC, all rights reserved.
+Copyright 2008-2014 Michael Greb, all rights reserved.
+Copyright 2008-2014 Linode, LLC, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
