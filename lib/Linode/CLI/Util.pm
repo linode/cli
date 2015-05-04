26b566340e370e58fb88295add4d643ea3922909
James Ottinger <jottinger@linode.com>
2015-05-04 15:02:15 -0400

    Release 1.4.6

--- lib/Linode/CLI/Util.pm
+++ lib/Linode/CLI/Util.pm
@@ -27,7 +27,7 @@ use Pod::Usage;
 use Getopt::Long (qw(:config no_ignore_case bundling pass_through));
 use Term::ANSIColor ':constants';
 
-our $VERSION = '1.4.5';
+our $VERSION = '1.4.6';
 our @MODES   = (qw(
     linode stackscript domain nodebalancer longview account user
 ));
